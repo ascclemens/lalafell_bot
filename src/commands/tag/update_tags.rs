af18b211dfd2c1bca378c5adabee66f45d36330d
Kyle Clemens <me@kyleclemens.com>
2017-06-07 21:11:34 -0400

    fix: allow updatetags outside of a channel

--- src/commands/tag/update_tags.rs
+++ src/commands/tag/update_tags.rs
@@ -3,7 +3,6 @@ use commands::*;
 use tasks::AutoTagTask;
 
 use discord::builders::EmbedBuilder;
-use discord::model::PublicChannel;
 
 use std::sync::Arc;
 
@@ -19,25 +18,9 @@ impl UpdateTagsCommand {
   }
 }
 
-impl HasBot for UpdateTagsCommand {
-  fn bot(&self) -> Arc<LalafellBot> {
-    self.bot.clone()
-  }
-}
-
-impl<'a> PublicChannelCommand<'a> for UpdateTagsCommand {
-  fn run(&self, message: &Message, channel: &PublicChannel, _: &[&str]) -> CommandResult<'a> {
-    let server_id = channel.server_id;
-    let state_option = self.bot.state.read().unwrap();
-    let state = state_option.as_ref().unwrap();
-    let server = match state.servers().iter().find(|x| x.id == server_id) {
-      Some(s) => s,
-      None => {
-        let err: error::Error = "could not find server for channel".into();
-        return Err(err.into());
-      }
-    };
-    if server.owner_id != message.author.id {
+impl<'a> Command<'a> for UpdateTagsCommand {
+  fn run(&self, message: &Message, _: &[&str]) -> CommandResult<'a> {
+    if !self.bot.config.bot.administrators.contains(&message.author.id.0) {
       return Err(ExternalCommandFailure::default()
         .message(|e: EmbedBuilder| e
           .title("Not enough permissions.")
