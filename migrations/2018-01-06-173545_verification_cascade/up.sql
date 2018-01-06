0df1f7d25b91ebce8c03b43c9e2c44a2ab0a1e24
Kyle Clemens <jkc.clemens@gmail.com>
2018-01-06 12:40:08 -0500

    chore: add database cascade

---
+++ migrations/2018-01-06-173545_verification_cascade/up.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys = OFF;
+ALTER TABLE verifications RENAME TO old_verifications;
+
+PRAGMA foreign_keys = ON;
+CREATE TABLE verifications (
+  id INTEGER PRIMARY KEY NOT NULL,
+  tag_id INTEGER NOT NULL,
+  verified BOOLEAN NOT NULL DEFAULT 'f',
+  verification_string VARCHAR,
+
+  FOREIGN KEY(tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+INSERT INTO verifications SELECT * FROM old_verifications;
+
+DROP TABLE old_verifications;
