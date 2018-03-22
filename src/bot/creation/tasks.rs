eca893f14b65e08ce0614a0f68d652eafde38e72
Kyle Clemens <me@kyleclemens.com>
2018-03-22 14:02:28 -0400

    fix: use default instead of new

--- src/bot/creation/tasks.rs
+++ src/bot/creation/tasks.rs
@@ -7,7 +7,7 @@ use std::sync::Arc;
 pub fn tasks(bot: &LalafellBot) -> Result<()> {
   let task_manager = TaskManager::new(Arc::clone(&bot.env));
   task_manager.start_task(AutoTagTask::new());
-  task_manager.start_task(TimeoutCheckTask::new());
+  task_manager.start_task(TimeoutCheckTask::default());
   task_manager.start_task(DeleteAllMessagesTask::new());
   task_manager.start_task(RandomPresenceTask::new(Arc::clone(&bot.discord.shard_manager)));
   task_manager.start_task(RoleCheckTask::new());
